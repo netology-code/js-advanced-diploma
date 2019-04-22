45ede8f59eca797f39e0350aa1fd5ca98ddbcb70
coursar <coursar@gmail.com>
2019-04-22 15:12:48 +0300

    fix event

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -212,7 +212,7 @@ export default class GamePlay {
       damageEl.classList.add('damage');
       cell.appendChild(damageEl);
 
-      damageEl.addEventListener('transitionend', () => {
+      damageEl.addEventListener('animationend', () => {
         cell.removeChild(damageEl);
         resolve();
       });
