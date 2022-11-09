45158803e166029eb07c93f1019fc5f34b7f2128
Tanya <voznyuk1998@mail.ru>
2022-11-10 00:39:00 +0300

    4 test

---
+++ src/js/__tests__/Character.js
@@ -0,0 +1,19 @@
+import Character from '../Character';
+import Daemon from '../Characters/Daemon';
+import Bowman from '../Characters/Bowman';
+import Magician from '../Characters/Magician';
+
+test('Создание объекта Character', () => {
+  expect(() => new Character()).toThrow('Нельзя создать объект класса Character');
+});
+
+ test('Создание персонажей должно быть без ошибок', () => {
+   expect(() => new Bowman()).not.toThrow();
+ });
+
+ test('Создание персонажей должно быть без ошибок', () => {
+   expect(() => new Magician()).not.toThrow();
+ });
+ test('Создание персонажей должно быть без ошибок', () => {
+   expect(() => new Daemon()).not.toThrow();
+ });
