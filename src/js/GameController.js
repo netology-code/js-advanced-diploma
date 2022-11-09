e7cce5849a3d34d7ab7de8f7fa9d3b69316b01ce
Tanya <voznyuk1998@mail.ru>
2022-11-09 23:54:36 +0300

    3

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,32 +1,177 @@
+import themes from './themes';
+import Team from './Team';
+import { generateTeam } from './generators';
+import Bowman from './Characters/Bowman';
+import Swordsman from './Characters/Swordsman';
+import Daemon from './Characters/Daemon';
+import Undead from './Characters/Undead';
+import Vampire from './Characters/Vampire';
+import Magician from './Characters/Magician';
+import PositionedCharacter from './PositionedCharacter';
+import GameState from './GameState';
+import GamePlay from './GamePlay';
+//import cursors from './cursors';
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.userTeam = new Team();
+    this.botTeam = new Team();
+    this.botCharacters = [Daemon, Undead, Vampire];
+    this.userCharacters = [Bowman, Swordsman, Magician];
+    this.gameState = new GameState();
   }
 
   init() {
-    this.gamePlay.drawUi('prairie');
+    this.gamePlay.drawUi(themes[this.gameState.level]);
+    this.userTeam.addAll(generateTeam([Bowman, Swordsman], 1, 2));
+    this.botTeam.addAll(generateTeam(this.botCharacters, 1, 2));
+    this.addsTheTeamToPosition(this.userTeam, this.getUserStartPositions());
+    this.addsTheTeamToPosition(this.botTeam, this.getBotStartPositions());
+    this.gamePlay.redrawPositions(this.gameState.allPositions);
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
 
-   
-
-    this.gamePlay.redrawPositions(this.players);
-
-    this.gamePlay.addCellEnterListener(this.onCellEnter);
-    this.gamePlay.addCellLeaveListener(this.onCellLeave);
-    this.gamePlay.addCellClickListener(this.onCellClick);
-    // TODO: add event listeners to gamePlay events
-    // TODO: load saved stated from stateService
+    GamePlay.showMessage(`Уровень ${this.gameState.level}`);
   }
 
-  onCellClick(index) {
-    // TODO: react to click
-  }
 
-  onCellEnter(index) {
-    // TODO: react to mouse enter
-  }
+   onCellClick(index) {
+  //   // TODO: react to click
+
+     if (this.gameState.level === 5 || this.userTeam.members.size === 0) {
+      return;
+     }
+
+  
+   }
+
+   onCellEnter(index) {
+  //   // TODO: react to mouse enter
+
+  
+   }
 
   onCellLeave(index) {
-    // TODO: react to mouse leave
   }
-}
+
+  
+  /**
+   * @returns {Array} Возвращает массив возможных позиций игрока при старте игры
+   */
+  getUserStartPositions() {
+    const size = this.gamePlay.boardSize;
+    this.userPosition = [];
+    for (let i = 0, j = 1; this.userPosition.length < size * 2; i += size, j += size) {
+      this.userPosition.push(i, j);
+    }
+    return this.userPosition;
+  }
+
+  /**
+   * @returns  массив возможных позиций бота при старте игры
+   */
+  getBotStartPositions() {
+    const size = this.gamePlay.boardSize;
+    const botPosition = [];
+    for (let i = size - 2, j = size - 1; botPosition.length < size * 2; i += size, j += size) {
+      botPosition.push(i, j);
+    }
+    return botPosition;
+  }
+
+  /**
+ * Возвращает рандомную позицию
+ * @param {Array} positions массив возможных позиций при старте игры
+ * @returns рандомное число
+ */
+  getRandom(positions) {
+    this.positions = positions;
+    return this.positions[Math.floor(Math.random() * this.positions.length)];
+  }
+
+  /**
+   * Добавляет команду в gameState.allPositions
+   * @param {Object} team команда (игрока или бота)
+   * @param {Array} positions массив возможных позиций при старте игры
+   */
+  addsTheTeamToPosition(team, positions) {
+    const copyPositions = [...positions];
+    for (const item of team) {
+      const random = this.getRandom(copyPositions);
+      this.gameState.allPositions.push(new PositionedCharacter(item, random));
+      copyPositions.splice(copyPositions.indexOf(random), 1);
+    }
+  }
+
+ 
+  /**
+   * Проверяет по индексу бота ли персонаж
+   * @param {number} idx индекс бота
+   * @returns boolean
+   */
+  isBotChar(idx) {
+    if (this.getChar(idx)) {
+      const bot = this.getChar(idx).character;
+      return this.botCharacters.some((elem) => bot instanceof elem);
+    }
+    return false;
+  }
+
+  /**
+   * @param {number} idx индекс персонажа
+   * @returns Возвращает персонажа по индексу из gameState.allPositions
+   */
+  getChar(idx) {
+    return this.gameState.allPositions.find((elem) => elem.position === idx);
+  }
+
+  /**
+   * Расчитывает диапазон перемещения или атаки
+   * @param {number} idx индекс персонажа
+   * @param {number} char значение свойства персонажа
+   * @returns возвращает массив валидных индексов
+   */
+  calcRange(idx, char) {
+    const brdSize = this.gamePlay.boardSize;
+    const range = [];
+    const leftBorder = [];
+    const rightBorder = [];
+
+    for (let i = 0, j = brdSize - 1; leftBorder.length < brdSize; i += brdSize, j += brdSize) {
+      leftBorder.push(i);
+      rightBorder.push(j);
+    }
+
+    for (let i = 1; i <= char; i += 1) {
+      range.push(idx + (brdSize * i));
+      range.push(idx - (brdSize * i));
+    }
+
+    for (let i = 1; i <= char; i += 1) {
+      if (leftBorder.includes(idx)) {
+        break;
+      }
+      range.push(idx - i);
+      range.push(idx - (brdSize * i + i));
+      range.push(idx + (brdSize * i - i));
+      if (leftBorder.includes(idx - i)) {
+        break;
+      }
+    }
+
+    for (let i = 1; i <= char; i += 1) {
+      if (rightBorder.includes(idx)) {
+        break;
+      }
+      range.push(idx + i);
+      range.push(idx - (brdSize * i - i));
+      range.push(idx + (brdSize * i + i));
+      if (rightBorder.includes(idx + i)) {
+        break;
+      }
+    }
+
+    return range.filter((elem) => elem >= 0 && elem <= (brdSize ** 2 - 1));
+  }}
+  
