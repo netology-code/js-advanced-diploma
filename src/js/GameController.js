029aab105840075a9fb4f94e1727e7604eca6150
DDefo25 <ispumamm@gmail.com>
2023-01-10 14:27:01 +0000

    Update GameController.js

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,14 +1,37 @@
-import themes from './themes';
+import themes from "./themes";
+import { generateTeam } from "./generators";
+import PositionedCharacter from "./PositionedCharacter";
+
+import Bowman from "./characters/Bowman";
+import Swordsman from "./characters/Swordsman";
+import Magician from "./characters/Magician";
+import Daemon from "./characters/Daemon";
+import Undead from "./characters/Undead";
+import Vampire from "./characters/Vampire";
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.charactersPositions = [];
   }
 
   init() {
     this.gamePlay.drawUi(themes.prairie);
+
+    const playerTypes = [Bowman, Swordsman, Magician];
+    const playerTeam = generateTeam(playerTypes, 3, 4);
+    this.positionTeam(playerTeam, [1, 2]);
+
+    const enemyTypes = [Daemon, Undead, Vampire];
+    const enemyTeam = generateTeam(enemyTypes, 3, 4);
+    this.positionTeam(enemyTeam, [7, 8]);
+
+    this.gamePlay.redrawPositions(this.charactersPositions);
+
     // TODO: add event listeners to gamePlay events
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+
     // TODO: load saved stated from stateService
   }
 
@@ -18,9 +41,51 @@ export default class GameController {
 
   onCellEnter(index) {
     // TODO: react to mouse enter
+    const character = this.getCharacter(index);
+    if (character) {
+      let { level, attack, defence, health } = character;
+      this.gamePlay.showCellTooltip(
+        `\u{1F396}${level} \u{2694}${attack} \u{1F6E1}${defence} \u{2764}${health}`,
+        index
+      );
+    }
   }
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
+  }
+
+  positionTeam(team, allowedColumns) {
+    const allowedCells = [];
+
+    allowedColumns.forEach((column) => {
+      for (let cell = 0; cell < this.gamePlay.boardSize ** 2; cell += 1) {
+        if (cell % this.gamePlay.boardSize === column - 1) {
+          allowedCells.push(cell);
+        }
+      }
+    });
+
+    team.characters.forEach((character) => {
+      this.charactersPositions.push(
+        new PositionedCharacter(character, this.getPosition(allowedCells))
+      );
+    });
+  }
+
+  getPosition(allowedCells) {
+    let position;
+    do {
+      position = allowedCells[Math.round(Math.random() * allowedCells.length)];
+    } while (this.getCharacter(position) || position === undefined);
+    return position;
+  }
+
+  getCharacter(position) {
+    const character = this.charactersPositions.find(
+      (character) => character.position === position
+    );
+    return character ? character.character : false;
   }
 }
